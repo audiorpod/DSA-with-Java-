bd9741e2f11b7d8c0da04e0151d2777e9d1d1421
Rupesh Sahu <rupeshsahu7541832256@gmail.com>
2023-02-06 12:07:50 +0530

    data type

---
+++ Data.java
@@ -0,0 +1,38 @@
+public class Data {
+    public static void main(String[] args) {
+        
+        int num1 = 2;
+        byte by= 127;
+        short sh = 558;
+        long l = 58554l;
+
+        float f =5.8f;
+        double d = 5.8;
+        
+        char c ='k';
+
+        boolean b = true;
+
+    
+
+        System.out.println(num1);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+    }
+    
+}
+
+
+// Data types - 2 type 1) - primitive types
+// Integer -> byte - 1 byte , short - 2 bytes , int -4 bytes, long - 8 bytes,
+
+// Float => float - 4 bytes , double - 8 bytes = in java double is in default because  double has more precession value eg - 45.9876453  ,
+// character => 2 bytes 
+// char c = 'k' in sting we use double couted and in character we use single couted 
+// Boolean => true , false , null ... it is generally used for conditional comparison 
+// boolean b = true ;
