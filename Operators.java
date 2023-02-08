9af541ccde90298ec311e500fc6efc46482a593b
Rupesh Sahu <91730407+audiorpod@users.noreply.github.com>
2023-02-08 12:30:13 +0530

    Update Operators.java

--- Operators.java
+++ Operators.java
@@ -2,7 +2,7 @@ public class Operators {
     public static void main(String[] args) {
         int num1 = 7;
         // int num = 5;
-        // int result = num1 % num2;    // for division slash is uded / and it will you  quotient 
+        // int result = num1 % num2;    // for division slash is uded / and it will give you  quotient 
           // and moduloud % will  given  you reminder 
           
           
@@ -10,16 +10,16 @@ public class Operators {
           //num1 = num1 + 2;   // adding number by itself 
           //num1 += 1;
           //num1 *= 2;
-          //num1++;      // post - increment 
-          //++num1; // pre - increment
-          //num1--;  // decrement   
+          //num1++;          // post - increment 
+          //++num1;          // pre - increment
+          //num1--;          // decrement   
           //or
              --num1;     // num1 --; => this is post - decrement 
                          //   and --num1  => this is pre - decrement  both are same ;
 //****** it behave differently we assigne the value or fetch the value so we need to dfefine it   */
         
-       //int result = num1++;  // output - 6  fetch the value and then increment  
-        int result =++num1; // output - 7   increament and the it will fetch the value 
+       //int result = num1++;        // output - 6  fetch the value and then increment  
+        int result =++num1;         // output - 7   increament and the it will fetch the value 
     System.out.println(result); 
         
     }
