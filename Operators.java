60cc8f456bb9968a552fe123a4045e0037f476c3
Rupesh Sahu <rupeshsahu7541832256@gmail.com>
2023-02-08 12:24:35 +0530

    Operators post - increment and pre-increment

---
+++ Operators.java
@@ -0,0 +1,27 @@
+public class Operators {
+    public static void main(String[] args) {
+        int num1 = 7;
+        // int num = 5;
+        // int result = num1 % num2;    // for division slash is uded / and it will you  quotient 
+          // and moduloud % will  given  you reminder 
+          
+          
+         
+          //num1 = num1 + 2;   // adding number by itself 
+          //num1 += 1;
+          //num1 *= 2;
+          //num1++;      // post - increment 
+          //++num1; // pre - increment
+          //num1--;  // decrement   
+          //or
+             --num1;     // num1 --; => this is post - decrement 
+                         //   and --num1  => this is pre - decrement  both are same ;
+//****** it behave differently we assigne the value or fetch the value so we need to dfefine it   */
+        
+       //int result = num1++;  // output - 6  fetch the value and then increment  
+        int result =++num1; // output - 7   increament and the it will fetch the value 
+    System.out.println(result); 
+        
+    }
+    
+}
